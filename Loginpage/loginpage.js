28c189560aaba91e612454b099a3c9aa9cf40afe
devpune1 <devpune1@bicnet.info>
2016-02-17 11:28:04 +0000

    new file

--- Loginpage/loginpage.js
+++ Loginpage/loginpage.js
@@ -1,36 +1,124 @@
-function clear()
+
+
+
+
+function reloadPage()
 {
     
-    document.getElementById("user_name").innerHTML="";
-    document.getElementById("user_pass").innerHTML="";
+     setTimeout(function () { location.reload(true); }, 500);
+    
+}
+
+function clearPage()
+{
+    
+    document.getElementById("user_name").value="";
+    document.getElementById("user_pass").value="";
     
 }
 
 
+function userReloadedPage()
+{
+    
+        
+        clearPage();
+    
+    
+    
+}
 
+
+function isEmpty(userdata)
+{
+    
+    if(userdata=="")
+    {
+        return false;
+        
+    }
+    else
+    {
+        
+        return true;
+    }
+    
+}
 
 function userDetail()
 {
-    
+  
     var userName=document.getElementById("user_name").value;
     
-    var userPass=document.getElementById("user_pass").value;
+    var userPass=document.getElementById("user_pass").value.replace(userPass,'*');
+     
     
     
-        if(userName==="asd" && userPass==="asd")
-        {
+    if(isEmpty(userName)==true && isEmpty(userPass)==true)
+    {
+        
+                
+            if(userName==="asd" && userPass==="asd")
+            {
             
                 alert("Your are WELCOMED");
-            
+                
+                clearPage();
                 window.location.href="../Userpage/userpage.html";
             
-        }    
-        else
+            }    
+            else
+            {
+            
+                alert("Your Not \"Authorized User\" ");
+                reloadPage();
+            }
+        
+       
+    }
+    else
+    {
+        
+        if(isEmpty(userName==false) )
         {
             
-            alert("Your Not \"Authorized User\" ");
-        }
-    
-    
+                document.getElementById("user_name").placeholder="please enter the name";
+                document.getElementById("user_name").style.borderWidth="2px";
+                document.getElementById("user_name").style.borderStyle="solid";
+                document.getElementById("user_name").style.borderColor="red";
+                reloadPage();
+        }   
+        
+           if(isEmpty(userPass==false))
+           {
+                 document.getElementById("user_pass").placeholder="please enter password here";
+                 document.getElementById("user_pass").textContent.color="black";
+                 document.getElementById("user_pass").style.borderWidth="2px";
+                 document.getElementById("user_pass").style.borderStyle="solid";
+                 document.getElementById("user_pass").style.borderColor="red";
+                 
+                reloadPage();
+           }
+           
+           
+           
+    }
     
 }   
+
+
+function onClosed()
+{
+    
+    
+    close();
+    
+}
+
+
+function newRegister()
+{
+    
+    
+    window.location.href="../Loginpage/Registrationpage/registration.html";
+}
